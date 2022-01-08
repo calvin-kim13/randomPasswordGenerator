6b3a3b0842bf9d048253dc316c38f92054ebd82e
Calvin Kim <calvinkim1230@gmail.com>
2022-01-08 06:09:23 -0800

    complete javascript and updated readme file with added screenshot

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,63 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Password Criteria
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var number = ["0123456789"];
+var special = ["!@#$%^&*+?<>/"];
+
+// generatePassword function created
+function generatePassword () {
+  var passwordLength = window.prompt("Enter your desired password length between 8-128 characters.");
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a valid password length between 8-128 characters.");
+
+    var passwordLength = window.prompt("Enter your desired password length between 8-128 characters.");
+  }
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert("Your password will be " + passwordLength + " characters long.");
+  }
+
+  var includeLowercase = window.confirm("Press 'Ok' if you would like to include lowercase characters in your password. Press 'Cancel' if you do not want to include it.");
+  var includeUppercase = window.confirm("Press 'Ok' if you would like to include uppercase characters in your password. Press 'Cancel' if you do not want to include it.");
+  var includeNumber = window.confirm("Press 'Ok' if you would like to include numbers in your password. Press 'Cancel' if you do not want to include it.");
+  var includeSpecial = window.confirm("Press 'Ok' if you would like to include special characters in your password. Press 'Cancel' if you do not want to include it.");
+
+  while (includeLowercase !== true && includeUppercase !== true && includeNumber !== true && includeSpecial !== true) {
+    window.alert("Error. You must select at least one option. Try again.");
+
+    var includeLowercase = window.confirm("Press 'Ok' if you would like to include lowercase characters in your password. Press 'Cancel' if you do not want to include it.");
+    var includeUppercase = window.confirm("Press 'Ok' if you would like to include uppercase characters in your password. Press 'Cancel' if you do not want to include it.");
+    var includeNumber = window.confirm("Press 'Ok' if you would like to include numbers in your password. Press 'Cancel' if you do not want to include it.");
+    var includeSpecial = window.confirm("Press 'Ok' if you would like to include special characters in your password. Press 'Cancel' if you do not want to include it.");
+  }
+
+  var passwordCriteria = []
+    if (includeLowercase === true) {passwordCriteria += lowercase};
+    if (includeUppercase === true) {passwordCriteria += uppercase};
+    if (includeNumber === true) {passwordCriteria += number};
+    if (includeSpecial === true) {passwordCriteria += special};
+
+  var randomizingPassword = ""
+    for (var i = 0; i < passwordLength; i++) {
+      randomizingPassword += passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
+    }
+
+  return randomizingPassword;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Password Generator
 
 ## Goal of the project
-The main goal of this project was to use what we learned with JavaScript and generate randomized passwords.
+- The main goal of this project was to use what we learned with JavaScript and generate randomized passwords. I had to give the user an option a choice between adding certain characters into their password and I also had to give the users a choice to select the length of their password. More specifically, the users had a choice to add lowercase, uppercase, numbers, and special characters into their password unless they chose not to include it. After receiving the input, I had to randomize the wanted characters and create a password that matches the chosen length as well.
+
+## Main Skills Applied
+- The main skills applied for this project was JavaScript.
 
 ## Screenshot of initial screen
 ![Alt text](/Assets/images/initial-image.png "Optional Title")
+
+## Screenshot of updated screen
+![Alt text](/Assets/images/password-generated-image.png "Optional Title")
+
 
 
